4131d73dabaef3ddda7980b648cba136e94f3382
maiko-ambitage <ambitage@outlook.jp>
2021-01-29 01:01:15 +0900

    ヘッダー部分テンプレート作成

--- app/controllers/dreamers_controller.rb
+++ app/controllers/dreamers_controller.rb
@@ -1,4 +1,8 @@
 class DreamersController < ApplicationController
   def index
   end
+  
+  def basic_auth
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'dreamers/index'
+ 
   root to: "dreamers#index"
+  resources :dreamers, only: [:index]
 end
