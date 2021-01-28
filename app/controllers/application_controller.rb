a81d10eae5c8d113816c7976333c2858905db00b
maiko-ambitage <ambitage@outlook.jp>
2021-01-29 00:45:34 +0900

    画像用gem導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :profile, :image, :genelation_id])
+  end
+
+ 
 end
