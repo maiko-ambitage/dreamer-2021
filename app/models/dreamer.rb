f891b3c258f02842790678b5d715a3fef9aa0f29
maiko-ambitage <ambitage@outlook.jp>
2021-02-02 01:24:32 +0900

    投稿完了まで

---
+++ app/models/dreamer.rb
@@ -0,0 +1,24 @@
+class Dreamer < ApplicationRecord
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :genre
+
+has_one_attached :image
+# has_one_attached :image
+belongs_to :user
+has_many :comments
+
+
+with_options presence: true do
+  validates :dream_title
+  validates :declaration
+  validates :period
+  validates :reward
+  validates :genre_id
+  
+end
+
+with_options numericality: { other_than: 1 } do
+  validates :genre_id
+end
+
+end
